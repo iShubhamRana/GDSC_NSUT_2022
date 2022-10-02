8b303fdef05c4b625479b83abd65023a71bc327d
Dhruv <dhruvdeora31@gmail.com>
2022-10-02 20:50:58 +0530

    alignment updated

--- src/Pages/Feed/index.jsx
+++ src/Pages/Feed/index.jsx
@@ -5,15 +5,20 @@ import sus from "../../Assets/Images/itachi.jpeg";
 export default function Feed() {
   return (
     <>
-      <div className="feed-container">
-        <div className="feed-title">Our Feed</div>
-        <div className="card-grid">
-          <FCard lol="https://picsum.photos/1080" />
-          <FCard lol="https://picsum.photos/1081" />
-          <FCard lol="https://picsum.photos/1082" />
-          <FCard lol="https://picsum.photos/1083" />
-          <FCard lol="https://picsum.photos/1084" />
-          <FCard lol="https://picsum.photos/1085" />
+      <div className="mainnn">
+        <div className="feed-container">
+          <div className="feed-title">Our Feed</div>
+          <div className="card-grid">
+            <FCard lol="https://picsum.photos/1080" />
+            <FCard lol="https://picsum.photos/1081" />
+            <FCard lol="https://picsum.photos/1082" />
+            <FCard lol="https://picsum.photos/1083" />
+            <FCard lol="https://picsum.photos/1084" />
+            <FCard lol="https://picsum.photos/1085" />
+            <FCard lol="https://picsum.photos/1086" />
+            <FCard lol="https://picsum.photos/1087" />
+            <FCard lol="https://picsum.photos/1088" />
+          </div>
         </div>
       </div>
     </>
